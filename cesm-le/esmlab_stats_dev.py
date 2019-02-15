48829a7326de623785d81b7872460b0e0abc5e09
matt-long <mclong@ucar.edu>
2019-02-15 16:38:01 -0700

    added cesm-le project

---
+++ cesm-le/esmlab_stats_dev.py
@@ -0,0 +1,26 @@
+import xarray as xr
+
+def weighted_mean_ds(ds, weights, dim):
+
+    if callable(weights):
+        weights = weights(ds)
+        print(weights)
+        
+    # copy time 
+    dso = xr.Dataset()
+    time_coord_name = esmlab.utils.time.infer_time_coord_name(ds)
+    tb_name, tb_dim = esmlab.utils.time.time_bound_var(ds, time_coord_name)
+    dso[tb_name] = ds[tb_name]
+    dso[time_coord_name] = ds[time_coord_name]
+    
+    # find variables where all `dim` appear (not sure we need to be this restrictive)
+    variable_list = [v for v, da in ds.variables.items() 
+                     if all(d in da.dims for d in dim)]
+
+    apply_nan_mask = True
+    for v in variable_list:
+        dso[v] = esmlab.statistics.weighted_mean(ds[v], weights=weights, dim=dim,
+                                                apply_nan_mask=apply_nan_mask)
+        apply_nan_mask = False
+    
+    return dso
